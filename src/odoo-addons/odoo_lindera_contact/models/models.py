7d3f94deb6262d07469947c21b674740f3b26c83
Ramin <ramin@Ramins-MacBook-Pro.local>
2019-09-27 15:34:17 +0200

    add: lindera custom module

---
+++ src/odoo-addons/odoo_lindera_contact/models/models.py
@@ -0,0 +1,84 @@
+from odoo import models, fields, api
+from openerp.osv import osv
+import pprint
+import inspect
+import requests as rq
+import os
+from cerberus import Validator
+
+INTERNAL_AUTHENTICATION_TOKEN = os.getenv("INTERNAL_AUTHENTICATION_TOKEN")
+URL = os.getenv("URL")
+
+
+class LinderaBackend(models.Model):
+    _inherit = 'res.partner'
+
+    @api.model
+    def create(self, val):
+        res = super(LinderaBackend, self).create(val)
+
+
+        def callLinderaAPI(data):
+            rq.post("{}/homes".format(URL), json=data, headers={'token': INTERNAL_AUTHENTICATION_TOKEN})
+
+        def validateData(data):
+            v = Validator()
+            schema = {
+                'name': {'type': 'string'}, # map
+                'role': {'type': 'string', 'allowed': ['home', 'company', 'organization']},
+                'street': {'type': 'string'},
+                'zip': {'type': 'string'},
+                'city':  {'type': 'string'},
+                # 'countrycodeISO316a2':  {'type': 'string'},  # map
+            }
+            result = v.validate(data, schema)
+            return result
+
+
+        tag = res.category_id
+        if tag:
+            tags = []
+            for item in tag:
+                tags.append(item.name)
+
+            # Check if 'Träger' is in the list
+            if 'Träger' in tags:
+                document = {
+                    'name': res.name,
+                    'city': res.city,
+                    'street': res.street,
+                    'zip': res.zip,
+                    'role': 'company'
+                }
+
+                if (validateData(document)):
+                    callLinderaAPI(document)
+                else:
+                    raise osv.except_osv(('Error!'), ('Invalid input data, address is missing'))
+
+             # Check if 'Einrichtung' is in the list
+            if 'Einrichtung' in tags:
+                document = {
+                    'name': res.name,
+                    'city': res.city,
+                    'street': res.street,
+                    'zip': res.zip,
+                    'role': 'home'
+                }
+                if (validateData(document)):
+                    callLinderaAPI(document)
+                else:
+                    raise osv.except_osv(('Error!'), ('Invalid input data, address is missing'))
+                
+            
+        else:
+            print("something")
+   
+    
+        return res
+
+
+
+class linderaApi(models.Model):
+    _name = "lindera.api"
+    _description = "lindera api related info"
