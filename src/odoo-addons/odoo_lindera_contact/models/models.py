a0c46af60e9a1cce2785fc0c2851ab85b3a73581
Ramin <ramin@Ramins-MacBook-Pro.local>
2019-10-11 13:44:03 +0200

    Enhanced: code

--- src/odoo-addons/odoo_lindera_contact/models/models.py
+++ src/odoo-addons/odoo_lindera_contact/models/models.py
@@ -1,4 +1,3 @@
-import logging
 from odoo import models, fields, api
 from openerp.osv import osv
 import pprint
@@ -6,60 +5,158 @@ import inspect
 import requests as rq
 import os
 from cerberus import Validator
+from raven import Client
 
-_logger = logging.getLogger(__name__)
+URL = 'http://localhost:3000/v2'
+INTERNAL_AUTHENTICATION_TOKEN = 'Bearer abc123'
+client = Client('https://2f93ec8aba4c419a836337bd8ff4b427:53d79797dd0642218c08b664581e4e6d@sentry.lindera.de/6')
 
-URL = 'https://backend-testing.lindera.de/v2/homes'
-INTERNAL_AUTHENTICATION_TOKEN = 'Bearer HfpWLjqt5k0YqIjPgYtb'
 
 class LinderaBackend(models.Model):
     _inherit = 'res.partner'
-
     @api.model
     def create(self, val):
         res = super(LinderaBackend, self).create(val)
 
+        isCompany = res.is_company
+        companyType = res.company_type
+        tag = res.category_id
+        parentId = res.parent_id.id
+        addressType = res.type
+
         # LINDERA API (homes resource)
         def postHome(data):
-            return rq.post("{}/homes".format(URL), json=data, headers={'token': INTERNAL_AUTHENTICATION_TOKEN})
+            try:
+                return rq.post("{}/homes".format(URL), json=data, headers={'token': INTERNAL_AUTHENTICATION_TOKEN})
+            except:
+                client.captureMessage('Lindera backend failed to create the resource (home)')
         
         def getHome(id):
-            return rq.get(URL+"/homes/?filter={}={}".format('odooID', id), headers={'token': INTERNAL_AUTHENTICATION_TOKEN})
+            try:
+                return rq.get(URL+"/homes?filter={}={}".format('odooID', id), headers={'token': INTERNAL_AUTHENTICATION_TOKEN})
+            except:
+                client.captureMessage('Lindera backend failed to fetch the resource (home)')
 
         def updateHome(id, data):
-            return rq.put("{}/homes/{}".format(URL, id), json=data, headers={'token': INTERNAL_AUTHENTICATION_TOKEN})
+            try:
+                return rq.put("{}/homes/{}".format(URL, id), json=data, headers={'token': INTERNAL_AUTHENTICATION_TOKEN})
+            except:
+                client.captureMessage('Lindera backend failed to update the resource (home)')
+
 
         
         # validator
         def validateData(data):
             v = Validator()
             schema = {
-                'name': {'type': 'string'}, # map
+                'name': {'type': 'string', 'empty': False }, # map
                 'role': {'type': 'string', 'allowed': ['home', 'company', 'organization']},
-                'street': {'type': 'string'},
-                'zip': {'type': 'string'},
-                'city':  {'type': 'string'},
+                'street': {'type': 'string', 'empty': False},
+                'zip': {'type': 'string', 'empty': False},
+                'city':  {'type': 'string', 'empty': False},
                 'odooID': {'type': 'number'},
                 'children': {'type': 'list'}
             }
             result = v.validate(data, schema)
             return result
-        
 
-        def linderaAPI(data):
-            if (validateData(data)):
-                result = postHome(data)
-                return result
+        
+        def preparePayload(tag):
+            data = {}
+            if tag == 'Einrichtung':
+                data = {
+                    'name': res.name,
+                    'city': res.city,
+                    'street': res.street,
+                    'zip': res.zip,
+                    'role': 'home',
+                    'odooID': res.id
+                }
+            if tag == 'Träger':
+                data = {
+                    'name': res.name,
+                    'city': res.city,
+                    'street': res.street,
+                    'zip': res.zip,
+                    'role': 'company',
+                    'odooID': res.id
+                }
+            return data
+
+        def createHome(payload):
+            result = validateData(payload)
+            if result:
+                postHome(payload)
             else:
                 raise osv.except_osv(('Error!'), ('Invalid input data, address is missing'))
-
-
-
         
-        isCompany = res.is_company
-        companyType = res.company_type
-        tag = res.category_id
-        parentId = res.parent_id.id
+        def process(tag, data):
+            if addressType and addressType == 'contact':
+                raise osv.except_osv(('Error!'), ('Address is missing'))
+            
+            response = getHome(parentId).json()
+
+            # If the resource (home/company/contact) is not in lindera backend, then create it
+            if response and response['total'] == 0:
+                result = self.env['res.partner'].search([('id','=',parentId)])
+                if result:
+                    if tag == 'Einrichtung':
+                        payload = {
+                            'name': result.name,
+                            'city': result.city,
+                            'street': result.street,
+                            'zip': result.zip,
+                            'role': 'company',
+                            'odooID': result.id
+                        }
+                    
+                    if tag == 'Träger':
+                        payload = {
+                            'name': result.name,
+                            'city': result.city,
+                            'street': result.street,
+                            'zip': result.zip,
+                            'role': 'organization',
+                            'odooID': result.id
+                        }
+                if validateData(payload):
+                    documentId = postHome(payload).json()['data']['_id']
+                else:
+                    raise osv.except_osv(('Error!'), ('Parent company does not have proper address'))
+
+                # Create the child resource (home/company/contact) and get the ID
+                if validateData(data):
+                    result = postHome(data).json()
+                
+                _id = result['data']['_id']
+                updatedField = {
+                    'children': [
+                        _id
+                    ]
+                }
+                # update the parent resource (children field) with new data
+                updateHome(documentId, updatedField) 
+
+            # If the resource (home/company/contact) exists in lindera backend, then update the children field with the newly created resource ID 
+            if response and response['total'] == 1 :
+                resourceid = response['data'][0]['_id']
+                currentValues = []
+                for child in response['data'][0]['children']:
+                    currentValues.append(child['_id'])
+                        
+                if validateData(data):
+                    result = postHome(data).json()
+
+                newResourceId = result['data']['_id']
+                currentValues.append(newResourceId)
+                        
+                updatedField = {
+                    'children': currentValues
+                }
+                # update children with ID
+                updateHome(resourceid, updatedField)
+            
+
 
         if isCompany and companyType == 'company':
             if len(tag) == 0:
@@ -79,81 +176,25 @@ class LinderaBackend(models.Model):
             if all(item in tags for item in first) or all(item in tags for item in second) or all(item in tags for item in third) or all(item in tags for item in forth):
                 raise osv.except_osv(('Error!'), ('These tags are not allowed to be used together'))
             
-        
+
             if 'Einrichtung' in tags:
-                # prepare data to send for lindera API
-                data = {
-                    'name': res.name,
-                    'city': res.city,
-                    'street': res.street,
-                    'zip': res.zip,
-                    'role': 'home',
-                    'odooID': res.id
-                }
+                payload = preparePayload('Einrichtung')
+                
+                if parentId:
+                    process('Einrichtung', payload)
+
+                else:
+                    createHome(payload)
+            
+            if 'Träger' in tags:
+                payload = preparePayload('Träger')
 
                 if parentId:
-                    if res.type == 'contact':
-                        raise osv.except_osv(('Error!'), ('Address is missing'))
-
-                    response = getHome(parentId).json()
-                    
-                    # If the resource (home/company/contact) is not in lindera backend, then create it
-                    if response and response['total'] == 0:
-                        result = self.env['res.partner'].search([('id','=',parentId)])
-                        doc = {
-                            'name': result.name,
-                            'city': result.city,
-                            'street': result.street,
-                            'zip': result.zip,
-                            'role': 'company',
-                            'odooID': result.id,
-                        }
-
-                        rid = postHome(doc).json()['data']['_id']
-
-                        returnedValue = linderaAPI(data).json()
-                        _id = returnedValue['data']['_id']
-
-
-                        updatedField = {
-                            'children': [
-                                _id
-                            ]
-                        }
-                        # update children with ID
-                        updateHome(rid, updatedField) 
-                    
-                    # If the resource exists, then update the children field with the newly created resource ID 
-                    if response and response['total'] == 1:
-                        
-                        resourceid = response['data'][0]['_id']
-                        currentValues = []
-                        for id in response['data'][0]['children']:
-                            currentValues.append(id['_id'])
-                            
-                        v = linderaAPI(data).json()
-                        oid = v['data']['_id']
-
-                        currentValues.append(oid)
-                        
-                        updatedField = {
-                            'children': currentValues
-                        }
-                        # update children with ID
-                        updateHome(resourceid, updatedField)
-                    
-                linderaAPI(data)
-
-            # if 'Träger' in tags:
-            #     # prepare data to send for lindera API
-            #     data = {
-            #         'name': res.name,
-            #         'city': res.city,
-            #         'street': res.street,
-            #         'zip': res.zip,
-            #         'role': 'company',
-            #         'odooID': res.id
-            #     }
+                    process('Träger', payload)
+                     
+                else:
+                    createHome(payload)
+            
         return res
   
 
