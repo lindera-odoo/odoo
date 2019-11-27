10c58e32db1a36a783b9184e369c3879af1a01d5
steffen.temme@lindera.de <steffen.temme@lindera.de>
2019-11-27 15:28:04 +0100

    more review

--- src/odoo-addons/Lindera-Modules /__init__.py
+++ src/odoo-addons/Lindera-Modules /__init__.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 from dotenv import load_dotenv  # noqa
 from pathlib import Path  # noqa
+# TODO: either add the specific version of dotenv to the requirements.txt or use a string for the path i.e. env_path = './custom-addons/.env'
+# since not all versions of dotenv accept POSIX paths...
 env_path = Path('./custom-addons') / '.env'  # noqa
 load_dotenv(dotenv_path=env_path)  # noqa
 
