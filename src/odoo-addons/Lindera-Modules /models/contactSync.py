f1353df84be0ecedcec93fec5714f0844af833ff
steffen.temme@lindera.de <steffen.temme@lindera.de>
2019-12-09 13:37:00 +0100

    contacts

--- src/odoo-addons/Lindera-Modules /models/contactSync.py
+++ src/odoo-addons/Lindera-Modules /models/contactSync.py
@@ -35,108 +35,11 @@ class CustomUser(models.Model):
 				try:
 					account = Account((CLIENT_ID, CLIENT_SECRET), token=token_backend)
 					if account.is_authenticated:
-						mailbox = account.mailbox()
-						inbox = list(
-							mailbox.inbox_folder().get_messages(limit=toCheck, batch=BATCH, download_attachments=True))
-						sent = list(
-							mailbox.sent_folder().get_messages(limit=toCheck, batch=BATCH, download_attachments=True))
+						partners = self.env['res.partner'].search([])
+						address_book = account.address_book()
+						for partner in partners:
 
-						messages = inbox + sent
-						messages = sorted(messages, key=lambda elem: elem.sent)
-
-						for message in messages:
-							########################### INBOX ##############################################################
-							if message in inbox:
-								# related partner might be weird without this...
-								if message.sender.address != syncUser.email:
-									contact = self.env['res.partner'].search([('email', "=", message.sender.address)])
-									if contact:
-										user = self.env['res.users'].search([('partner_id', "=", contact[0].id)])
-										if not user:
-											mail = self.env['mail.message'].search([('subject', '=', message.subject),
-											                                        ('date', '=', message.sent),
-											                                        ('email_from', '=',
-											                                         contact[0].email)])
-											if not mail:
-												attachments = []
-												for attachment in message.attachments:
-													attachment = self.env['ir.attachment'].create({
-														'datas': attachment.content.encode(),
-														'name': attachment.name,
-														'datas_fname': attachment.name})
-													self.env.cr.commit()
-													attachments.append(attachment.id)
-												self.env['mail.message'].create({
-													'subject': message.subject,
-													'date': message.sent,
-													'body': message.body_preview,
-													'email_from': contact[0].email,
-													'attachment_ids': [[6, 0, attachments]],
-													'model': 'res.partner',
-													'res_id': contact[0].id,
-													'author_id': contact[0].id,
-													'message_type': 'email'
-												})
-												self.env.cr.commit()
-							################################ SENT ##########################################################
-							if message in sent:
-								author = self.env['res.users'].search([('email', "=", message.sender.address)])
-								for recipient in message.to:
-									# related partner might be weird without this...
-									if recipient.address != syncUser.email:
-										contact = self.env['res.partner'].search([('email', "=", recipient.address)])
-										if contact:
-											user = self.env['res.users'].search([('partner_id', "=", contact[0].id)])
-											if not user:
-												# check if date
-												assert isinstance(message.sent, datetime.datetime), 'Must be a date!'
-												self.env.cr.execute(
-													"SELECT id FROM mail_message WHERE ABS(EXTRACT(EPOCH FROM (date::timestamp - '" + str(
-														message.sent)[:-6] + "'::timestamp))) < 2")
-												self.env.cr.execute("SELECT id FROM mail_message ")
-												results = self.env.cr.fetchall()
-												mail = False
-												for res in results:
-													mail = self.env['mail.message'].search(
-														[('subject', '=', message.subject),
-														 ('id', '=', res[0]),
-														 ('res_id', '=', contact[0].id)])
-													if mail:
-														break
-
-												if not mail:
-													attachments = []
-													for attachment in message.attachments:
-														attachment = self.env['ir.attachment'].create({
-															'datas': attachment.content.encode(),
-															'name': attachment.name,
-															'datas_fname': attachment.name})
-														self.env.cr.commit()
-														attachments.append(attachment.id)
-													if author:
-														self.env['mail.message'].create({
-															'subject': message.subject,
-															'date': message.sent,
-															'body': message.body_preview,
-															'email_from': message.sender.address,
-															'attachment_ids': [[6, 0, attachments]],
-															'model': 'res.partner',
-															'res_id': contact[0].id,
-															'author_id': author[0].commercial_partner_id.id,
-															'message_type': 'email'
-														})
-													else:
-														self.env['mail.message'].create({
-															'subject': message.subject,
-															'date': message.sent,
-															'body': message.body_preview,
-															'email_from': message.sender.address,
-															'attachment_ids': [[6, 0, attachments]],
-															'model': 'res.partner',
-															'res_id': contact[0].id,
-															'message_type': 'email'
-														})
-													self.env.cr.commit()
+							contact = address_book.new_contact()
 				except Exception as err:
 					ravenSingle.Client.captureMessage(err)
 					raise osv.except_osv('Error While Syncing!', str(err))
