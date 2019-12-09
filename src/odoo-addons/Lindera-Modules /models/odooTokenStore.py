ac586b630171b55f8c36a4f3b5f72ef030941ec8
steffen.temme@lindera.de <steffen.temme@lindera.de>
2019-12-09 09:28:47 +0100

    Forgot to add some stuff to git...

---
+++ src/odoo-addons/Lindera-Modules /models/odooTokenStore.py
@@ -0,0 +1,25 @@
+from O365.utils import BaseTokenBackend
+
+
+class odooTokenStore(BaseTokenBackend):
+	def __init__(self, user):
+		self._user = user
+		super(odooTokenStore, self).__init__()
+
+	def get_token(self):
+		self.token = self._user.auth_token
+		return self.token
+
+	def save_token(self):
+		self._user.auth_token = self.token
+		return True
+
+	def delete_token(self):
+		self._user.auth_token = ''
+		return True
+
+	def check_token(self):
+		try:
+			return self._user.auth_token != ''
+		except:
+			return False
