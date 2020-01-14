7c3f873095f9a1b7f6da66eab34472b984133f51
steffen.temme@lindera.de <steffen.temme@lindera.de>
2020-01-14 10:43:22 +0100

    fixed tech@lindera.odoo.de bug and conversations

--- src/odoo-addons/Lindera-Modules /models/__init__.py
+++ src/odoo-addons/Lindera-Modules /models/__init__.py
@@ -7,6 +7,7 @@ from . import mailSync
 from . import userAdditions
 from . import officeAuth
 from . import mail_mail
+from . import mail_message
 from . import contactSync
 from . import calendar_event
 from . import calendarSync
