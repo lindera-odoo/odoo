72a55248cf48dc47c5615772b2d432b3e8f1ef94
steffen.temme@lindera.de <steffen.temme@lindera.de>
2020-02-03 14:29:38 +0100

    resetting the token if email or login changes

--- src/odoo-addons/Lindera-Modules /models/userAdditions.py
+++ src/odoo-addons/Lindera-Modules /models/userAdditions.py
@@ -1,4 +1,4 @@
-from odoo import models, fields
+from odoo import models, fields, api
 
 class LinderOfficeUser(models.Model):
     """
@@ -9,4 +9,10 @@ class LinderOfficeUser(models.Model):
     auth_state = fields.Char('auth_state')
     auth_token = fields.Char('auth_token')
     auth_url = fields.Char('auth_url')
+
+    @api.multi
+    def write(self, values):
+        if 'email' in values.keys() or 'login' in values.keys():
+            values['auth_token'] = ''
+        return super(LinderOfficeUser, self).write(values)
 
