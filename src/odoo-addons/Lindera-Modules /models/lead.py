d15574d3852be198f52a6954b0fa75ccb13d1bd2
steffen.temme@lindera.de <steffen.temme@lindera.de>
2020-03-05 12:48:17 +0100

    test gui

--- src/odoo-addons/Lindera-Modules /models/lead.py
+++ src/odoo-addons/Lindera-Modules /models/lead.py
@@ -15,6 +15,14 @@ class Linderlead(models.Model):
 	                               help='String Containing Senior Number or empty string',
 	                               store=True)
 
+	start_date = fields.Date('start_date')
+	end_date = fields.Date('end_date')
+
+	date_string = fields.Char(compute='_compute_date_string',
+	                          help='String Containing the start and end Date of a Contract',
+	                          store=True)
+	show_dates = fields.Boolean('show_senior_number')
+
 	@api.depends('partner_id', 'partner_id.senior_number', 'partner_id.show_senior_number')
 	def _compute_senior_number(self):
 		for lead in self:
@@ -28,6 +36,18 @@ class Linderlead(models.Model):
 	def _compute_senior_number_string(self):
 		for lead in self:
 			if lead.senior_number != 0:
-				lead.senior_number_string = 'mit ' + str(lead.senior_number) + ' Senioren'
+				lead.senior_number_string = 'mit ' + str(lead.senior_number) + ' Senioren\n'
 			else:
 				lead.senior_number_string = ''
+
+	@api.depends('start_date', 'end_date')
+	def _compute_date_string(self):
+		for lead in self:
+			lead.show_dates = True
+			if lead.start_date and lead.end_date:
+				# TODO: check for Versicherung tag
+				lead.show_dates = True
+				lead.date_string = str(lead.start_date) + ' - ' + str(lead.end_date)
+			else:
+				lead.date_string = ''
+				lead.show_dates = False
