215de25c4e89d7f9a735595f2d7363eb4678382b
steffen.temme@lindera.de <steffen.temme@lindera.de>
2020-06-16 11:51:46 +0200

    gathering intel

--- src/odoo-addons/Lindera-Modules /models/lindera_ticket.py
+++ src/odoo-addons/Lindera-Modules /models/lindera_ticket.py
@@ -5,19 +5,11 @@ _logger = logging.getLogger(__name__)
 
 class LinderaTicket(models.Model):
     _inherit = 'helpdesk.ticket'
-    _logger.warning('inherited ticket')
 
-    @api.model
-    def write(self, values):
-        _logger.warning('Dumping Data for Logs since odoo never heard from debugging.... WRITING')
-        _logger.warning(str(values))
-        res = super(LinderaTicket, self).write(values)
-        _logger.warning('More Dumping')
-        _logger.warning(str(res))
-        _logger.warning('Dumping Messages')
-        _logger.warning(str(self.message_ids))
-        
-        if '@lindera' in self.partner_email and 'partner_email' not in values.keys():
+    @api.onchange('message_ids')
+    def _onchange_messages(self):
+        _logger.warning('Dumping Data for Logs since odoo never heard from debugging.... Message')
+        if '@lindera' in self.partner_email:
             for message in self.message_ids:
                 try:
                     _logger.warning(message)
@@ -25,6 +17,4 @@ class LinderaTicket(models.Model):
                     test = json.loads(message.body)
                 except:
                     pass
-        
         _logger.warning('Dumping Done')
-        return res
