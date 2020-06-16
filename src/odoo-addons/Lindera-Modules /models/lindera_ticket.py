1595473857325f3e214370bb5db5514ab0421761
steffen.temme@lindera.de <steffen.temme@lindera.de>
2020-06-16 10:59:10 +0200

    gathering intel

--- src/odoo-addons/Lindera-Modules /models/lindera_ticket.py
+++ src/odoo-addons/Lindera-Modules /models/lindera_ticket.py
@@ -1,14 +1,18 @@
 from odoo import models, fields, api
 
+import logging
+_logger = logging.getLogger(__name__)
+
 class LinderaTicket:
     _inherit = 'helpdesk.ticket'
+    _logger.warning('inherited ticket')
 
     @api.model
     def create(self, val):
-        print('Dumping Data for Logs since odoo never heard from debugging....')
-        raise val
-        # res = super(LinderaTicket, self).create(val)
-        # print('More Dumping')
-        # print(res)
-        # print('Dumping Done')
-        # return res
+        _logger.warning('Dumping Data for Logs since odoo never heard from debugging....')
+        _logger.warning(str(val))
+        res = super(LinderaTicket, self).create(val)
+        _logger.warning('More Dumping')
+        _logger.warning(str(res))
+        _logger.warning('Dumping Done')
+        return res
