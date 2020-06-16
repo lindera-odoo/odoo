1b1587de31bef9ceb035be5ae2d92234af82a2e2
steffen.temme@lindera.de <steffen.temme@lindera.de>
2020-06-16 15:11:01 +0200

    first test

--- src/odoo-addons/Lindera-Modules /models/mail_message.py
+++ src/odoo-addons/Lindera-Modules /models/mail_message.py
@@ -24,28 +24,28 @@ class linderaMail(models.Model):
                 _logger.warning(str(ticket.partner_email))
                 _logger.warning('@lindera' in str(ticket.partner_email))
                 if '@lindera' in str(ticket.partner_email):
-                    try:
-                        clean = re.sub('<.*?>', '', str(self.body))
-                        _logger.warning(clean)
-                        data = json.loads(clean)
-                        partner = self.env['res.partner'].search([('email', '=', data['email'])])
-                        if not partner:
-                            create_data = {
-                                'name': data['contact_name'],
-                                'email': data['email']
-                            }
-                            if 'street' in data.keys(): create_data['street'] = data['address']
-                            if 'zip_city' in data.keys(): create_data['zip'] = data['zip_city'].split(' ')[0]
-                            if 'zip_city' in data.keys() and ' ' in data['zip_city']:
-                                create_data['city'] = data['zip_city'].split(' ')[1]
-                            
-                            partner = self.env['res.partner'].create(create_data)
-                        _logger.warning(partner)
-                        ticket.partner_email = partner.email
-                        ticket.partner_id = partner.id
-                        self.env.cr.commit()
-                    except Exception as e:
-                        _logger.warning(e)
+                    # try:
+                    clean = re.sub('<.*?>', '', str(self.body))
+                    _logger.warning(clean)
+                    data = json.loads(clean)
+                    partner = self.env['res.partner'].search([('email', '=', data['email'])])
+                    if not partner:
+                        create_data = {
+                            'name': data['contact_name'],
+                            'email': data['email']
+                        }
+                        if 'street' in data.keys(): create_data['street'] = data['address']
+                        if 'zip_city' in data.keys(): create_data['zip'] = data['zip_city'].split(' ')[0]
+                        if 'zip_city' in data.keys() and ' ' in data['zip_city']:
+                            create_data['city'] = data['zip_city'].split(' ')[1]
+                        
+                        partner = self.env['res.partner'].create(create_data)
+                    _logger.warning(partner)
+                    ticket.partner_email = partner.email
+                    ticket.partner_id = partner.id
+                    self.env.cr.commit()
+                    # except Exception as e:
+                    #     _logger.warning(e)
 
                 
         _logger.warning('Logging Message create end')
