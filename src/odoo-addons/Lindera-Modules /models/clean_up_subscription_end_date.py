c4be9244b16179b4caa5dbad36e6c87b7caa1eae
steffen.temme@lindera.de <steffen.temme@lindera.de>
2022-07-25 18:17:58 +0200

    adds stage check to cron

--- src/odoo-addons/Lindera-Modules /models/clean_up_subscription_end_date.py
+++ src/odoo-addons/Lindera-Modules /models/clean_up_subscription_end_date.py
@@ -8,7 +8,7 @@ class linderaConversationsCleaner(models.Model):
     def clean_subscription_end_date(self):
         to_clean = self.env['crm.lead'].search([])
         for lead in to_clean:
-            if lead.partner_id.homeID is not None and lead.end_date:
+            if lead.partner_id.homeID is not None and lead.end_date and lead.stage_id.allow_subscription:
                 lead.partner_id.updateHome(lead.partner_id.homeID,
                                            {
                                                'subscriptionEndDate': lead.end_date.isoformat()
