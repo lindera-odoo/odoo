80d1eb0461273753889d69e66e1f83ade7d19e53
steffen.temme@lindera.de <steffen.temme@lindera.de>
2024-01-15 14:19:51 +0100

    fix reference?

--- src/odoo-addons/Lindera-Modules /models/home.py
+++ src/odoo-addons/Lindera-Modules /models/home.py
@@ -1,5 +1,5 @@
 from odoo import models, fields, api
-from .form_of_address import form_of_address_selection_options
+from . import form_of_address
 
 class LinderaHome(models.Model):
 	"""
@@ -12,7 +12,7 @@ class LinderaHome(models.Model):
 	
 	first_name = fields.Char(compute='_compute_form_of_address', readonly=False)
 	last_name = fields.Char(compute='_compute_form_of_address', readonly=False)
-	form_of_address = fields.Selection(selection=form_of_address_selection_options, compute='_compute_form_of_address', readonly=False)
+	form_of_address = fields.Selection(selection=form_of_address.form_of_address_selection_options, compute='_compute_form_of_address', readonly=False)
 	
 	full_address  = fields.Char(compute='_compute_full_form_of_address')
 
@@ -100,7 +100,7 @@ class LinderaHome(models.Model):
 	def _compute_full_form_of_address(self):
 		for partner in self:
 			if partner.form_of_address == 'mixed':
-				partner.full_address = dict(form_of_address_selection_options)['mixed']
+				partner.full_address = dict(form_of_address.form_of_address_selection_options)['mixed']
 			elif partner.form_of_address in ['woman', 'man']:
-				partner.full_address = dict(form_of_address_selection_options)[partner.form_of_address] + ' ' + partner.last_name
+				partner.full_address = dict(form_of_address.form_of_address_selection_options)[partner.form_of_address] + ' ' + partner.last_name
 				
